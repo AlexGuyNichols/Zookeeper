571d9d011804158da808a8745ffde454b31ca4b7
alexguynichols <alexguynichols@daxta.co.uk>
2022-08-10 15:39:24 +0100

    Added finishing touches, made the animals display as a sectioned list, grouped by animal types

--- Zookeeper/ContentView.swift
+++ Zookeeper/ContentView.swift
@@ -8,17 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	let animalGroups = [
+		AnimalGroup(groupName: "Pets", animals: [
+			Animal(name: "Dog"),
+			Animal(name: "Cat"),
+			Animal(name: "Parrot"),
+			Animal(name: "Mouse")
+		]),
+		AnimalGroup(groupName: "Farm", animals: [
+			Animal(name: "Cow"),
+			Animal(name: "Horse"),
+			Animal(name: "Goat"),
+			Animal(name: "Sheep"),
+		]),
+		AnimalGroup(groupName: "Critters", animals: [
+			Animal(name: "Firefly"),
+			Animal(name: "Spider"),
+			Animal(name: "Ant"),
+			Animal(name: "Squirrel"),
+		])
+	]
+	
     var body: some View {
-		VStack{
-			Text("I love list").font(.headline)
+		VStack {
 			List {
-				Text("Item 1")
-				Text("Item 2")
-				Text("Item 3")
-			}
-			.listStyle(InsetGroupedListStyle())
+				ForEach(animalGroups) {
+					animalGroup in Section(header: Text(animalGroup.groupName)) {
+						ForEach(animalGroup.animals) {
+							animal in Text(animal.name)
+						}
+					}
+				}
+			} .listStyle(InsetGroupedListStyle())
+			//Button("Add") {
+				//animals.append(Animal(name: "New Animal"))
+			//}
 		}
     }
+}
+
+struct Animal: Identifiable {
+	var id = UUID()
+	var name: String
+}
+
+struct AnimalGroup: Identifiable {
+	var id = UUID()
+	var groupName : String
+	var animals : [Animal]
 }
 
 struct ContentView_Previews: PreviewProvider {
