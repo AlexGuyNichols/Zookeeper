e032ed5ebd792665092b7c5436efe8a3594a5062
alexguynichols <alexguynichols@daxta.co.uk>
2022-08-10 15:09:32 +0100

    Initial Commit

---
+++ Zookeeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zookeeper
+//
+//  Created by Alex Nichols on 10/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Alex Nichols on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
